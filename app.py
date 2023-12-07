1c0388b00d1c21311879f6ef8a72c7839aaec4d2
kauex55 <kauelima2477@gmail.com>
2023-12-07 15:51:51 -0300

    up

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+# Exemplo de dados (substitua isso por seu mecanismo de armazenamento real)
+agendamentos = []
+
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', agendamentos=agendamentos)
 
 @app.route('/agendar', methods=['GET', 'POST'])
 def agendar():
@@ -12,10 +15,31 @@ def agendar():
         # Lógica para agendar horário
         data = request.form['data']
         horario = request.form['horario']
-        # ... processamento do agendamento ...
+        novo_agendamento = {'data': data, 'horario': horario}
+        agendamentos.append(novo_agendamento)
         return jsonify({'message': 'Horário agendado com sucesso!'})
 
     return render_template('agendar.html')
+
+@app.route('/agendamentos/<int:agendamento_id>', methods=['DELETE', 'PUT'])
+def gerenciar_agendamento(agendamento_id):
+    if request.method == 'DELETE':
+        if 0 <= agendamento_id < len(agendamentos):
+            del agendamentos[agendamento_id]
+            return jsonify({'message': 'Agendamento excluído com sucesso!'})
+        else:
+            return jsonify({'error': 'ID de agendamento inválido'}), 404
+
+    elif request.method == 'PUT':
+        # Lógica para atualizar agendamento
+        if 0 <= agendamento_id < len(agendamentos):
+            data = request.form['data']
+            horario = request.form['horario']
+            agendamentos[agendamento_id]['data'] = data
+            agendamentos[agendamento_id]['horario'] = horario
+            return jsonify({'message': 'Agendamento atualizado com sucesso!'})
+        else:
+            return jsonify({'error': 'ID de agendamento inválido'}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
--- script.js
+++ script.js
@@ -4,18 +4,56 @@ document.getElementById('agendamentoForm').addEventListener('submit', function (
     const data = document.getElementById('data').value;
     const horario = document.getElementById('horario').value;
 
-    fetch('./agendar.html', {
-        method: 'POST',
+    // Verificar se é uma operação de criação ou atualização
+    const agendamentoId = document.getElementById('agendamentoId').value;
+    const method = agendamentoId ? 'PUT' : 'POST';
+    const url = agendamentoId ? `/agendamentos/${agendamentoId}` : '/agendar';
+
+    fetch(url, {
+        method: method,
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: JSON.stringify({ data, horario }),
+        body: new URLSearchParams({
+            data: data,
+            horario: horario
+        })
     })
     .then(response => response.json())
     .then(data => {
         alert(data.message);
+
+        // Adicione um elemento para exibir o recado na página
+        const recadoDiv = document.createElement('div');
+        recadoDiv.innerText = 'Horário agendado com sucesso!';
+        recadoDiv.className = 'recado';
+        document.body.appendChild(recadoDiv);
+
+        // Limpar os campos do formulário após o agendamento
+        document.getElementById('agendamentoForm').reset();
     })
     .catch(error => {
         console.error('Erro:', error);
     });
 });
+
+function excluirAgendamento(agendamentoId) {
+    fetch(`/agendamentos/${agendamentoId}`, {
+        method: 'DELETE',
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+        // Atualizar a página ou realizar outras ações necessárias
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+    });
+}
+
+function editarAgendamento(agendamentoId, data, horario) {
+    document.getElementById('agendamentoId').value = agendamentoId;
+    document.getElementById('data').value = data;
+    document.getElementById('horario').value = horario;
+    document.getElementById('submitBtn').innerText = 'Atualizar';
+}
